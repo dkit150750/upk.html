75964ec8516a9e5f801ddc6a4da8303fc8423939
dkit150750 <dkit150750@gmail.com>
2021-07-15 23:54:56 +0300

    color-0

--- admin/core.js
+++ admin/core.js
@@ -7,12 +7,15 @@ links.forEach((link) => {
 });
 
 const page = document.querySelector('.page');
+const body = document.querySelector('.page__body');
 const openNavBtn = document.querySelector('.main__header-btn');
 openNavBtn.addEventListener('click', () => {
   page.classList.add('show-menu');
+  body.classList.add('page__body--lock');
 });
 
 const closeNavBtn = document.querySelector('.aside__header-btn');
 closeNavBtn.addEventListener('click', () => {
   page.classList.remove('show-menu');
+  body.classList.remove('page__body--lock');
 });
